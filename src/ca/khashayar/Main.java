0ec5e77283fb2c9209364546863614dbc6820fc4
khashayarghorbani <khashayar.ghorbani@itdcanada.ca>
2019-09-30 11:23:24 -0700

    int short byte variables values

--- src/ca/khashayar/Main.java
+++ src/ca/khashayar/Main.java
@@ -3,6 +3,18 @@ package ca.khashayar;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+	// diffrent variables
+   // byte short int long
+        byte byteNumber = Byte.MAX_VALUE;
+        short shortNumber = Short.MAX_VALUE;
+        int intNumberMin = Integer.MIN_VALUE;
+        int intNumberMax = Integer.MAX_VALUE;
+        byte Bytenumbers=60;
+        byte otherByte = (byte)(Bytenumbers/2);
+        System.out.println("maximum number of integer is " + intNumberMax );
+        System.out.println("minimum number of integer is " + intNumberMin );
+        System.out.println("Byte number is " + byteNumber );
+        System.out.println("Short number is " + shortNumber );
+        System.out.println(otherByte);
+   }
 }
